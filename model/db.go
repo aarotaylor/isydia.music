0427de07faf983b11838dd70a753b0e52f209d3a
Aaron Taylor <ghost57924@gmail.com>
2026-06-04 20:10:10 -0700

    not *quite* ready for objectbox yet

---
+++ model/db.go
@@ -0,0 +1,16 @@
+package model
+
+// ObjectBox initialization
+// func InitBedRock() (*objectbox.ObjectBox, error) {
+// 	objectBox, err := objectbox.NewBuilder().Model(model.ObjectBoxModel()).Build()
+// 	if err != nil {
+// 		fmt.Println("Error initializing objectbox: ", err.Error())
+// 		return nil, err
+// 	}
+// 	return objectBox, nil
+// }
+
+// func GetObjectBox() (*objectbox.ObjectBox, error) {
+
+// }
+//
