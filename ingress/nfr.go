16c525725b4da5fbf9d43c829e4df918087b81bc
Aaron Taylor <ghost57924@gmail.com>
2026-06-04 20:13:20 -0700

    getting ready for objectbox

---
+++ ingress/nfr.go
@@ -0,0 +1,12 @@
+package narrative
+
+const (
+	NFPATH = "./Narrative/"
+)
+
+// This module will read in the Narrative file directory, populating objectbox, and contains
+// other functions and methods that will be related to reading narrative files
+// toggle the preference for file vs objectbox copy;
+func NarrativeIngress() {
+
+}
